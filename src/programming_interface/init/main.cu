e519ba491d6e13ec47577aa3424cffae5da75601
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2024-09-25 23:51:54 +0900

    初期化の例の追加など
    Signed-off-by: Yuya Asano <64895419+sukeya@users.noreply.github.com>

---
+++ src/programming_interface/init/main.cu
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <cstdio>
+
+#include <cuda_runtime.h>
+
+int main() {
+    // Instruct CUDA to yield its thread when waiting for results from the device.
+    unsigned int device_flags = cudaDeviceScheduleYield;
+    // Tell the CUDA runtime that DeviceFlags is being set in cudaInitDevice call 
+    unsigned int flags = cudaInitDeviceFlagsAreValid;
+    // Initialize device to be used for GPU executions.
+    auto e = cudaInitDevice(0, device_flags, flags);
+    assert(e == cudaSuccess);
+
+    // Set device to be used for GPU executions.
+    e = cudaSetDevice(0);
+    assert(e == cudaSuccess);
+    
+    int device;
+    e = cudaGetDevice(&device);
+    assert(e == cudaSuccess);
+    assert(device == 0);
+
+    unsigned int current_flags;
+    e = cudaGetDeviceFlags(&current_flags);
+    assert(e == cudaSuccess);
+    assert((current_flags & device_flags) == device_flags);
+}
